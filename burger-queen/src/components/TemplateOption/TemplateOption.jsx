d78e951164712f69e28d1f60bc08f8bbc289a051
Mariia <maria.bookvoed@gmail.com>
2019-07-15 12:40:25 -0400

    crear archivo json con estructura de datos

--- burger-queen/src/components/TemplateOption/TemplateOption.jsx
+++ burger-queen/src/components/TemplateOption/TemplateOption.jsx
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import './TemplateOption.css';
-
+import Waiter from '../../img/iconomesero.png';
+import Chef from '../../img/iconococinero.png'
 class TemplateOption extends Component {
     render(){
         return(
             <div className="container TemplateOption"  id="TemplateOption">
                 <div className="row">
                     <div className="col-6 mesero">
-                        <button type="button" className="btn btn-mesero">Meser@</button>
+                        <button type="button" className="btn btn-mesero"><img className="img-fluid img-mesero" src={Waiter} alt="Waiter"/></button>
                     </div>
                     <div className="col-6 cocinero">
-                        <button type="button" className="btn btn-cocinero">Cociner@</button>
+                        <button type="button" className="btn btn-cocinero"><img className="img-fluid img-cocinero" src={Chef} alt=""/></button>
                     </div>
                 </div>
             </div>
