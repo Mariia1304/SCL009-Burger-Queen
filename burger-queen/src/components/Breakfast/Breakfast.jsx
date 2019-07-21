11aa8616de9898205bd4e7fc126d7d861bd3319f
Mariia <maria.bookvoed@gmail.com>
2019-07-21 02:18:23 -0400

    Render Button con map de json

--- burger-queen/src/components/Breakfast/Breakfast.jsx
+++ burger-queen/src/components/Breakfast/Breakfast.jsx
@@ -9,7 +9,6 @@ class Breakfast extends Component{
             <div className="desayuno col-12">
                 <div className="row">
                     {Menu.breakfast.map(el=><div className="col-6"><Button key={el.item} class={el.class} image={el.image} price={el.price} item={el.item} /></div>)}
-                   
                 </div>
             </div>
         )
--- burger-queen/src/components/Button/Button.jsx
+++ burger-queen/src/components/Button/Button.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import images from '../../importImages';
 class Button extends React.Component{
     render(){
-      return <button className= "btn item"><img className={this.props.class} src={images[this.props.image]}/><p>{this.props.item}<br/>{this.props.price}</p></button>
+      return <button className= "btn item" onClick={()=>this.props.itemAndPriceToOrder(this.props.item, this.props.price)} >
+                <img className={this.props.class} src={images[this.props.image]} alt={this.props.item}/>
+                <p>{this.props.item}<br/>{this.props.price}</p>
+             </button>
     }
 }
 export default Button;
