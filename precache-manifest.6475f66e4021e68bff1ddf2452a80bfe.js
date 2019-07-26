ef0e49147d25e1484f4e4cdd77e2130da08b3884
Mariia <maria.bookvoed@gmail.com>
2019-07-26 00:18:55 -0400

    Updates

---
+++ precache-manifest.6475f66e4021e68bff1ddf2452a80bfe.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a524698bc918c4a4f249f9d7ba6a31c7",
+    "url": "/SCL009-Burger-Queen/index.html"
+  },
+  {
+    "revision": "c7cbb48a40252a1436a7",
+    "url": "/SCL009-Burger-Queen/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "32f15e10f0abffd7fb95",
+    "url": "/SCL009-Burger-Queen/static/css/main.892aaf80.chunk.css"
+  },
+  {
+    "revision": "c7cbb48a40252a1436a7",
+    "url": "/SCL009-Burger-Queen/static/js/2.208dab5a.chunk.js"
+  },
+  {
+    "revision": "32f15e10f0abffd7fb95",
+    "url": "/SCL009-Burger-Queen/static/js/main.99f82a56.chunk.js"
+  },
+  {
+    "revision": "e81fd8658db00d469a46",
+    "url": "/SCL009-Burger-Queen/static/js/runtime~main.bfb50733.js"
+  },
+  {
+    "revision": "a5ebf8e3f100eda63092eab4563872a8",
+    "url": "/SCL009-Burger-Queen/static/media/cheese.a5ebf8e3.png"
+  },
+  {
+    "revision": "9928aa873704e522e31a6e94bef68f64",
+    "url": "/SCL009-Burger-Queen/static/media/coffee-cup.9928aa87.svg"
+  },
+  {
+    "revision": "7718853170a1c80de7bb363417b806b2",
+    "url": "/SCL009-Burger-Queen/static/media/coffee.77188531.svg"
+  },
+  {
+    "revision": "d4798137877b0297bc4e25c161c7f101",
+    "url": "/SCL009-Burger-Queen/static/media/delete.d4798137.svg"
+  },
+  {
+    "revision": "bfa1f5f6721db51af69526690fc7fb17",
+    "url": "/SCL009-Burger-Queen/static/media/egg3.bfa1f5f6.png"
+  },
+  {
+    "revision": "9e5bed6bfffc60bd2209ffc6c9464f6b",
+    "url": "/SCL009-Burger-Queen/static/media/egg4.9e5bed6b.png"
+  },
+  {
+    "revision": "538098afe8b6e266a4315b4f93d1c31c",
+    "url": "/SCL009-Burger-Queen/static/media/fried-potatoes.538098af.png"
+  },
+  {
+    "revision": "7c195ba790bc8fb683c9480930dc43a1",
+    "url": "/SCL009-Burger-Queen/static/media/fried-potatoes.7c195ba7.svg"
+  },
+  {
+    "revision": "6ec1a978f38c88023928a792b0be43ca",
+    "url": "/SCL009-Burger-Queen/static/media/logo.6ec1a978.png"
+  },
+  {
+    "revision": "4560aecb15d6225f8d0e0bf08ff1ce44",
+    "url": "/SCL009-Burger-Queen/static/media/onion-rings.4560aecb.svg"
+  },
+  {
+    "revision": "57647f913d15a33d13a13dc33a35d5a6",
+    "url": "/SCL009-Burger-Queen/static/media/onion-rings.57647f91.png"
+  },
+  {
+    "revision": "9ab214888767730d201e5f1f764436e3",
+    "url": "/SCL009-Burger-Queen/static/media/orange-juice.9ab21488.svg"
+  },
+  {
+    "revision": "fff6b4a3c06ecae12e61db3258256c80",
+    "url": "/SCL009-Burger-Queen/static/media/water.fff6b4a3.svg"
+  }
+]);
